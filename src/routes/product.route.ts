4b247e7afc0b6fa8587d4659acb61bcb45065df4
Isabela Costa <isa-hcosta@hotmail.com>
2023-02-06 19:14:24 -0300

    cria validação dos produtos

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import ProductController from '../controllers/product.controller';
+import ProductMiddleware from '../middlewares/product.middleware';
 
 const router = Router();
 
 const controller = new ProductController();
 
 router.get('/products', controller.findAll);
+
+router.use(ProductMiddleware.validateAmount);
+router.use(ProductMiddleware.validateName);
 router.post('/products', controller.create);
 
 export default router;

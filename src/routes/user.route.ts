4d2dc3313c2af326d9d81262b21fb26e79271b34
Isabela Costa <isa-hcosta@hotmail.com>
2023-02-06 20:07:47 -0300

    cria validação dos users

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/user.controller';
+import UserMiddleware from '../middlewares/user.middleware';
 
 const router = Router();
 
 const controller = new UserController();
 
+router.use(UserMiddleware.validateUsername);
+router.use(UserMiddleware.validateVocation);
+router.use(UserMiddleware.validateLevel);
+router.use(UserMiddleware.validatePassword);
 router.post('/', controller.create);
 
 export default router;

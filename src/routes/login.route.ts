aa63e75de184339bb9fbd2cacfa2caeb9d974111
Isabela Costa <isa-hcosta@hotmail.com>
2023-02-06 18:45:16 -0300

    arruma nome do arquivo

--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/user.controller';
-import LoginMiddleware from '../middlewares/logign.middleware';
+import LoginMiddleware from '../middlewares/login.middleware';
 
 const router = Router();
 
